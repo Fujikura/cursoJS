9c95edf46b466aabf8fb8bf634daac6530e086a9
Marcio Fujikura <marciofujikura@hotmail.com>
2021-04-17 15:29:04 -0300

    82 - Método Splice

---
+++ 82 - Método Splice/main.js
@@ -0,0 +1,49 @@
+//const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Julia'];
+
+//nomes.splice(indice, delete, addElement1, addElement2);
+
+//remove um elemento do indice 4
+//const removidos = nomes.splice(4, 1);
+//console.log(nomes, removidos);
+
+//remove um elemento contando de tras pra frente 
+// const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Julia'];
+// const removidos = nomes.splice(-2, 1);
+// console.log(nomes, removidos);
+
+//Adiciona Luiz no indice 3 e não remove nada
+// const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Julia'];
+// const removidos = nomes.splice(3, 0, 'Luiz');
+// console.log(nomes, removidos);
+
+//Adiciona Luiz no indice 3 removendo o Gabriel
+// const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Julia'];
+// const removidos = nomes.splice(3, 1, 'Luiz');
+// console.log(nomes, removidos);
+
+//remove Gabriel e Julia e adiciona o Luiz
+// const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Julia'];
+// const removidos = nomes.splice(3, 2, 'Luiz');
+// console.log(nomes, removidos);
+
+//simulando o POP
+// const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Julia'];
+// const removidos = nomes.splice(-1, 1);
+// console.log(nomes, removidos);
+
+//simulando o SHIFT
+// const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Julia'];
+// const removidos = nomes.splice(0, 1);
+// console.log(nomes, removidos);
+
+
+//simulando o PUSH
+// const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Julia'];
+// nomes.splice(nomes.length, 0, 'Luiz');
+// console.log(nomes);
+
+
+//simulando o UNSHIFT
+// const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Julia'];
+// nomes.splice(0, 0, 'Luiz');
+// console.log(nomes);
